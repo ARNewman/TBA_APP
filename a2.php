16b3378e8a19fd184e35404e149c47d0fc5f9eb8
ARNewman <mytribes@hotmail.com>
2015-04-29 16:48:49 +1000

    Create a2.php

---
+++ a2.php
@@ -0,0 +1,47 @@
+<?php
+
+ini_set('display_errors', 1);
+//import the database connection script
+require("../private/db.connect.php");
+
+$query2 = "SELECT s.id AS id, s.ht_id AS team_id, s.play_date, s.Court, t.name
+FROM schedule s, team t
+WHERE s.ht_id=t.id AND s.comp_id=2
+UNION
+SELECT s.id, s.at_id AS team_id, s.play_date, s.Court, t.name
+FROM schedule s, team t
+WHERE s.at_id=t.id AND s.comp_id=2
+ORDER BY id ASC";
+
+$sth = $pdo->query($query2);
+
+$game = array();
+
+$a2 = array();
+
+$i = count($sth);
+
+while ($first = $sth->fetch(PDO::FETCH_ASSOC)){
+
+	if($i % 2 == 0){
+		
+		$game["away"]=$first["name"];
+		
+		array_push($a2, $game);
+		
+	}
+	
+	else{
+		$game["date"]=$first["play_date"];
+		$game["court"]=$first["Court"];
+		$game["home"]=$first["name"];
+		
+	}
+	
+	$i++;
+	
+	
+}
+echo json_encode($a2);
+
+?>
